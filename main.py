99efff5e2bdc0fbc19bc161c76b4d10075d45d45
Igiv Igiv <gregoire.igor@gmail.com>
2023-02-14 17:15:10 +0100

    0.8

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
-import numpy as np
+from board import P4
 
-### Player A : 1
-### Player B : -1
+jeu = P4()
+jeu.start()
 
+loop = True
+playerA = 1
+playerB = -1
+player = 1
 
-class P4:
-    def __init__(self):
-        self.rows = 7
-        self.columns = 6
-        self.matrix = np.array([])
-
-    def start(self):
-        self.matrix = np.zeros((self.columns, self.rows))
-
-    def slide(self,x_position, player):
-        y_position = self.columns -1
-        while self.matrix[y_position, x_position] != 0:
-            y_position -= 1
-            if y_position == -1:
-                return False
-        self.add((x_position, y_position), player)
-
-    def add(self, position, value):
-        self.matrix[position[1], position[0]] = value
-
-    def test(self):
-        if self.x_test() or self.y_test():
-            pass
-
-    def x_test(self):
-        for i in self.matrix:
-            for j in range(len(i)-3):
-                if i[j:4] == np.ones(4) or i[j:4] == -np.ones(4):
-                    return True
-        return False
-
-    def y_test(self):
-        pass
-
-
-    def __str__(self):
-        return str(self.matrix)
-
-
-board = P4()
-board.start()
-print(board)
+print(jeu)
+while loop:
+    print("Player",player,"what's your next action ?")
+    act = input("Action : ")
+    if not jeu.slide(int(act),player):
+        print(jeu)
+    else:
+        player *= -1
+        print(jeu)
